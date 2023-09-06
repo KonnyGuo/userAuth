7295698a6e486830651948e7c638dd12f79c88fa
KonnyGuo <guokonny@gmail.com>
2023-09-05 22:06:44 -0400

    ejs added

--- Auth/route.js
+++ Auth/route.js
@@ -3,8 +3,11 @@ const router = express.Router()
 const {register, login, update, deleteUser} = require("./auth")
 router.route("/register").post(register)
 router.route("/login").post(login)
+const adminAuth = require("../middleware/tokenCheck")
 //updates are put req, (crud) while the top are post
-router.route("/update").put(update)
-router.route("/deleteUser").delete(deleteUser)
+//adminAuth to protect update and delete privileges to admin
+router.route("/update").put(adminAuth, update)
+router.route("/deleteUser").delete(adminAuth, deleteUser)
+
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const connectDB = require("./db")
 const cookieParser = require("cookie-parser")
 const {adminAuth, basicAuth} = require("./middleware/tokenCheck")
 
+app.set("view engine", ejs)
+
 const PORT = 8000
 
 connectDB()
+
+app.get("/", (req, res) => {res.render("home")})
+app.get("/register", (req, res) => {res.render("register")})
+app.get("/login", (req, res) => {res.render("login")})
+app.get("/admin", adminAuth, (req, res) => {res.render("admin")})
+app.get("/basic", basicAuth, (req, res) => {res.render("user")})
+
 //when recieving reques to api/Auth go to .Auth/route middleware
 app.use(express.json())
 app.use("/api/Auth", require("./Auth/route"))
